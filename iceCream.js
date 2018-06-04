6ccdc3ac187a0d2aca5349ff0508cbe0c51dcc00
Yahya <yahya.mused@student.mec.cuny.edu>
2018-06-04 16:24:46 -0400

    code fixed

--- iceCream.js
+++ iceCream.js
@@ -1,4 +1,7 @@
-/* Write a single if statement that logs out the message:
+/*
+ * Programming Quiz: Ice Cream (3-6)
+ *
+ * Write a single if statement that logs out the message:
  * 
  * "I'd like two scoops of __________ ice cream in a __________ with __________."
  * 
@@ -12,8 +15,13 @@
  */
 
 // change the values of `flavor`, `vessel`, and `toppings` to test your code
-var flavor = "strawberry";
+var flavor = "vanilla";
 var vessel = "cone";
-var toppings = "cookies";
+var toppings = "peanuts";
 
-// Add your code here
+
+if ((flavor === 'vanilla' || flavor === 'chocolate') &&
+    (vessel === 'cone' || vessel === 'bowl') &&
+    (toppings === 'sprinkles' || toppings === 'peanuts')) {
+    console.log('I\'d like two scoops of ' + flavor + ' ice cream in a ' + vessel + ' with ' + toppings + '.');
+}
